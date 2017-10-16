30b1bd7c57f06b021cba86b64409671c55fe285a
eadrwlo <wlodek14a@gmail.com>
2017-10-16 22:55:32 +0200

    Selection of the best chromosome for new population.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,29 +3,39 @@ package com.company;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Main {
-	
-	
+public class Main
+{
+    public static void main(String[] args)
+    {
+        final int POPULATE_SIZE = 10;
 
-    public static void main(String[] args) {
-        int POPULATE_SIZE = 10;
-
-        Population population1 = new Population(10);
-        Population population2 = new Population(10);
+        Population population1 = new Population(POPULATE_SIZE);
+        Population population2 = new Population(POPULATE_SIZE);
         population1.calculateEachChromoSelectionProbability(); 
-       
+
+
         population2.crossChromosome(population1);
         population2.calculateEachChromoSelectionProbability();      
-               
+
+        Population population3 = new Population(population1.selectAndGetBestAdaptedChromosomes());
+
+
         System.out.println("Populacja przed krzyzowaniem:");
             for (Chromosome i : population1.getChromosomes()) {
                 System.out.println(i.getPhenotype() + "\t" + i.getBinaryPhenotype()
-                                    + "\t" + i.getAdaptationEval() + "\t" + i.getSelectionProbability());
+                                    + "\t" + i.getAdaptationEval() + "\t" + i.getSelectionProbability() + "ch id:" + i.getChromosomeId());
         }
             System.out.println("Populacja po krzyzowaniu:");
             for (Chromosome j : population2.getChromosomes()) {
                 System.out.println(j.getPhenotype() + "\t" + j.getBinaryPhenotype()
-                                    + "\t" + j.getAdaptationEval() + "\t" + j.getSelectionProbability());
+                                    + "\t" + j.getAdaptationEval() + "\t" + j.getSelectionProbability() + "ch id:" + j.getChromosomeId());
         }
+
+        System.out.println("Populacja 1 po wybraniu najlepszych:");
+        for (Chromosome j : population3.getChromosomes()) {
+            System.out.println(j.getPhenotype() + "\t" + j.getBinaryPhenotype()
+                    + "\t" + j.getAdaptationEval() + "\t" + j.getSelectionProbability() + "ch id:" + j.getChromosomeId());
+        }
+
     }
 }
